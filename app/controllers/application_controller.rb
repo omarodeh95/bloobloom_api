3c9e781b083a0f21391dab052241949ff6cd545d
Omar Odeh <omar.odeh95@hotmail.com>
2022-11-16 07:52:39 +0300

    adding JWT encode/decode methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
 class ApplicationController < ActionController::API
+
   include ::ActionController::Cookies
+
+  def encode_token(payload)
+    JWT.encode(payload, 'secret key here')
+  end
+
+  def decode_token()
+    auth_header = request.headers['Authorization']
+    if auth_header
+        token = auth_header.split(' ')[1]
+        begin
+          JWT.decode(token, 'secret key here', true, algorithm: "HS256")
+        rescue JWT::DecodeError
+          nil
+        end
+    end
+  end
+
+    def authorized_user
+      decoded_token = decode_token()
+      if decoded_token
+        user_id = decoded_token[0]
+        @user = User.find(user_id)
+      end
+    end
+
+    def authorize_user
+      render json: {msg: "You have to login first"}, status: :unauthorized unless authorized_user
+    end
 end
